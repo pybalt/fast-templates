2fe8440b8489ea41d3db0098bf080c3a3388feed
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2024-04-01 16:49:43 -0300

    change url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='FastAPI-TemplateWizard',
     version='0.1.0',
     packages=find_packages(),
-    url='https://github.com/pybalt/FastCLI',
+    url='https://github.com/pybalt/FastAPI-TemplateWizard',
     author='pybalt',
     author_email='96897286+pybalt@users.noreply.github.com',
     description='A CLI to generate FastAPI templates',
