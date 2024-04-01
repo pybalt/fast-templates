f50c57fda831bc0a1d85bac71b236d452c2aa8e7
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2024-04-01 16:59:50 -0300

    change file location & extend it to description

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='FastAPI-TemplateWizard',
-    version='0.1.0',
+    version='0.1.2',
     packages=find_packages(),
     url='https://github.com/pybalt/FastAPI-TemplateWizard',
     author='pybalt',
     author_email='96897286+pybalt@users.noreply.github.com',
     description='A CLI to generate FastAPI templates',
+    long_description=long_description,
     install_requires=[
         'typer',
         'questionary'
