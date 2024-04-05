4897caee5bf2b7e3eeb266fa1b939eeac1147584
Leonel B. Bravo <leobravolbb2002bej@gmail.com>
2024-04-05 10:33:36 -0300

    v0.2.1

--- setup.py
+++ setup.py
@@ -4,22 +4,18 @@ with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='fast-templates',
-    version='0.2.0',
+    name="fast-templates",
+    version="0.2.1",
     packages=find_packages(),
-    package_data={'': ['**/*', '.env', '.gitignore']},
-    url='https://github.com/pybalt/FastAPI-TemplateWizard',
-    author='pybalt',
-    author_email='96897286+pybalt@users.noreply.github.com',
-    description='A CLI to generate FastAPI templates',
+    package_data={"": ["**/*", ".env", ".gitignore"]},
+    url="https://github.com/pybalt/FastAPI-TemplateWizard",
+    author="pybalt",
+    author_email="96897286+pybalt@users.noreply.github.com",
+    description="A CLI to generate FastAPI templates",
     long_description=long_description,
-    install_requires=[
-        'typer',
-        'questionary',
-        'pyyaml'
-    ],
-    entry_points='''
+    install_requires=["typer", "questionary", "pyyaml"],
+    entry_points="""
         [console_scripts]
         fastcli=fasttemplates.main:app
-    '''
+    """,
 )
