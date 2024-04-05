1a097a4f4eec234f9e12536d106e71d983167f47
Leonel B. Bravo <leobravolbb2002bej@gmail.com>
2024-04-05 10:32:48 -0300

    update import errors

--- fasttemplates/main.py
+++ fasttemplates/main.py
@@ -2,9 +2,6 @@ import os
 
 import typer
 import questionary
-import tempfile
-import shutil
-import stat
 import yaml
 
 from fasttemplates.constants import *
@@ -14,8 +11,9 @@ app = typer.Typer()
 
 
 def custom() -> None:
-    options = questionary.checkbox("What do you want to add?", choices=[
-        REDIS_PROMPT, MONGO_PROMPT]).ask()
+    options = questionary.checkbox(
+        "What do you want to add?", choices=[REDIS_PROMPT, MONGO_PROMPT]
+    ).ask()
     mongo_present = MONGO_PROMPT in options
     redis_present = REDIS_PROMPT in options
     add_base_layout()
@@ -30,7 +28,7 @@ def custom() -> None:
         if cache:
             add_cache_with_redis()
             print("Redis cache added")
-    
+
     create_docker_compose()
 
 
@@ -42,21 +40,22 @@ def popular() -> None:
         FASTAPI_DJANGO_ORM: lambda: clone_and_copy(FASTAPI_WITH_DJANGO_ORM),
     }
 
-    option = questionary.select("Which popular template do you want to clone?", choices=[
-        FASTAPI_FS, FASTAPI_DJANGO_ORM]).ask()
+    option = questionary.select(
+        "Which popular template do you want to clone?",
+        choices=[FASTAPI_FS, FASTAPI_DJANGO_ORM],
+    ).ask()
 
     optionLambdas[option]()
 
     print("Popular template added")
 
+
 @app.command()
 def main() -> None:
-    popular_or_simplified_functions = {
-        'Popular': popular,
-        'Custom': custom
-    }
-    popular_or_simplified = questionary.select("Popular templates or custom templates?", choices=[
-        'Popular', 'Custom']).ask()
+    popular_or_simplified_functions = {"Popular": popular, "Custom": custom}
+    popular_or_simplified = questionary.select(
+        "Popular templates or custom templates?", choices=["Popular", "Custom"]
+    ).ask()
     popular_or_simplified_functions[popular_or_simplified]()
 
 
@@ -74,8 +73,8 @@ def add_mongo() -> None:
     with open(os.path.join(WORKING_DIR, "requirements.txt"), "a") as f:
         f.write("\ndnspython\npymongo\n")
 
-    docker_compose_dict['services'].update(mongo_service)
-    docker_compose_dict['volumes'].update(mongo_volume)
+    docker_compose_dict["services"].update(mongo_service)
+    docker_compose_dict["volumes"].update(mongo_volume)
 
     add_mongo_uri_to_settings()
 
@@ -84,13 +83,14 @@ def create_docker_compose() -> None:
     with open(os.path.join(WORKING_DIR, "docker-compose.yml"), "w") as f:
         yaml.dump(docker_compose_dict, f, default_flow_style=False, sort_keys=False)
 
+
 def add_redis() -> None:
     REDIS_DIR = os.path.join(WORKING_DIR, DB_DIR)
     os.makedirs(REDIS_DIR, exist_ok=True)
     copy_files(DB_TREE, REDIS_DIR, ignore_dirs=[MONGO_DIR_NAME])
 
-    docker_compose_dict['services'].update(redis_service)
-    docker_compose_dict['volumes'].update(redis_volume)
+    docker_compose_dict["services"].update(redis_service)
+    docker_compose_dict["volumes"].update(redis_volume)
 
     add_redis_uri_to_settings()
 
--- fasttemplates/utils.py
+++ fasttemplates/utils.py
@@ -1,7 +1,9 @@
 import os
 import shutil
+import tempfile
+import stat
 
-from fasttemplates.constants import WORKING_DIR
+from fasttemplates.constants import WORKING_DIR, SETTINGS_DIR
 
 
 def copy_files(src_dir, dst_dir, ignore_dirs=None):
@@ -21,40 +23,52 @@ def copy_files(src_dir, dst_dir, ignore_dirs=None):
         else:
             shutil.copy2(src_item, dst_item)
 
+
 def cout_settings_properties() -> int:
     settings_path = os.path.join(WORKING_DIR, "app\\settings.py")
     with open(settings_path, "r") as f:
         lines = f.readlines()
-    
-    class_def_index = next(i for i, line in enumerate(
-        lines) if SETTINGS_CLASS_DEFINITION in line)
-    
-    return len([line for line in lines if line.strip() and not line.strip().startswith('#')])+1
+
+    return (
+        len(
+            [
+                line
+                for line in lines
+                if line.strip() and not line.strip().startswith("#")
+            ]
+        )
+        + 1
+    )
+
 
 def add_property_to_settings(_K: str, _T: type, _V: str) -> None:
     settings_path = os.path.join(WORKING_DIR, SETTINGS_DIR)
     with open(settings_path, "r") as f:
         lines = f.readlines()
-    
+
     NUMBER_OF_PROPERTIES = cout_settings_properties()
-    lines.insert(NUMBER_OF_PROPERTIES, f'    {_K}: {_T.__name__} = {_V}\n')
+    lines.insert(NUMBER_OF_PROPERTIES, f"    {_K}: {_T.__name__} = {_V}\n")
 
     with open(settings_path, "w") as f:
         f.writelines(lines)
 
+
 def add_mongo_uri_to_settings():
-    add_property_to_settings('MONGO_URI', str, "'mongodb://localhost:27017'")
+    add_property_to_settings("MONGO_URI", str, "'mongodb://localhost:27017'")
+
 
 def add_redis_uri_to_settings():
-    add_property_to_settings('REDIS_URI', str, "'redis://localhost:6379/0'")
+    add_property_to_settings("REDIS_URI", str, "'redis://localhost:6379/0'")
+
 
 def on_rm_error(func, path, exc_info) -> None:
     "Error handler for `shutil.rmtree`"
     os.chmod(path, stat.S_IWRITE)
     os.unlink(path)
 
+
 def clone_and_copy(repo_url: str) -> None:
     with tempfile.TemporaryDirectory() as tmpdirname:
-        os.system(f'git clone {repo_url} {tmpdirname}')
-        shutil.rmtree(os.path.join(tmpdirname, '.git'), onerror=on_rm_error)
+        os.system(f"git clone {repo_url} {tmpdirname}")
+        shutil.rmtree(os.path.join(tmpdirname, ".git"), onerror=on_rm_error)
         copy_files(tmpdirname, WORKING_DIR)
